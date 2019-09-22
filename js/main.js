aeb36b0783810e28255804913b4c3bedd2e8ee45
Shubhadeep <41886925+shubha161@users.noreply.github.com>
2019-09-22 19:56:35 +0530

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,52 @@
+function $ChBg(){
+	var chbg = document.getElementById('Uname');
+	chbg.style.borderBottom="2px solid #fff";
+	chbg.style.transition="all 0.5s";
+	chbg.style.background="transparent";
+	chbg.style.color="#fff"
+}
+
+function $Chpass(){
+	var chbg = document.getElementById('Pass');
+	chbg.style.borderBottom="2px solid #fff";
+	chbg.style.transition="all 0.5s";
+	chbg.style.background="transparent";
+	chbg.style.color="#fff"
+}
+
+function $cHbG(){
+	var chbg = document.getElementById('Uname');
+	chbg.style.transition="all 1s";
+	chbg.style.background="white";
+	chbg.style.color="#717171"
+}
+
+function $cHPass(){
+	var chbg = document.getElementById('Pass');
+	chbg.style.transition="all 1s";
+	chbg.style.background="white";
+	chbg.style.color="#717171"
+}
+
+// button
+
+function $btn(){
+	var chbg = document.getElementById('Btn');
+	chbg.style.border="2px solid #000";
+	chbg.style.transition="all 0.3s";
+	chbg.style.background="linear-gradient(0deg, #FF00CC, #8678E9)";
+	chbg.style.color="#000";
+	chbg.style.fontSize="19px";
+}
+
+function $obtn(){
+	var chbg = document.getElementById('Btn');
+	chbg.style.border="none";
+	chbg.style.borderRadius="20px";
+	chbg.style.borderBottom="2px solid #fff";
+	chbg.style.borderTop="2px solid #fff";
+	chbg.style.transition="all 0.3s";
+	chbg.style.background="linear-gradient(180deg, #FF00CC, #8678E9)";
+	chbg.style.color="#fff"
+	chbg.style.fontSize="18px";
+}
